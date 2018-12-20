4cd33286ea9e27e61899c5fafd2ee61f95d9fc86
Park-JunHyung <schwazodiac@gmail.com>
2018-12-20 17:33:49 +0900

    Add getOpcode, getLabel in Assemble

--- src/myclass/Assemble.java
+++ src/myclass/Assemble.java
@@ -1,5 +1,7 @@
 package myclass;
 
+import myenum.opcode;
+
 public class Assemble {
     int LABELSIZE = 10;
     private int instrCnt;
@@ -9,16 +11,44 @@ public class Assemble {
     private char label[];
     public int startAddr;
 
+    public static String[] opcodeName={
+            "notop", "neg",  "inc", "dec", "dup", "swp", "add",
+            "sub",   "mult", "div", "mod", "and", "or",  "gt",
+            "lt",    "ge",   "le",  "eq",  "ne",  "lod", "ldc",
+            "lda",   "ldi",  "ldp", "str", "sti", "ujp", "tjp",
+            "fjp",   "call", "ret", "retv","chkh","chkl","nop",
+            "proc",  "end",  "bgn", "sym", "dump","none"
+    };
+
     public Assemble() {
         instrCnt = 0;
     }
 
     private void getLabel() {
-        
+        int i;
+        while (Character.isSpaceChar(lineBuffer[bufIndex]))
+            bufIndex++;
+        for (i = 0; i <= LABELSIZE && !Character.isSpaceChar(label[i] = lineBuffer[bufIndex]); bufIndex++, i++) ;
+        label[i] = '\0';
     }
 
+
     private int getOpcode() {
-        return 0;
+        char mnemonic[] = new char[5];
+        int index;
+
+        bufIndex = 11;
+        index = 0;
+        while (index < 5 && !Character.isSpaceChar(lineBuffer[bufIndex]))
+            mnemonic[index++] = lineBuffer[bufIndex++];
+        mnemonic[index] = '\0';
+
+        for (index = opcode.notop.ordinal(); index<opcode.none.ordinal();index++)
+            if (String.valueOf(mnemonic).equals(opcodeName[index]))
+                break;
+        if (index==opcode.none.ordinal())
+            System.err.println("error Illegal opcode, "+mnemonic);
+        return index;
     }
 
     private int getOperand() {
