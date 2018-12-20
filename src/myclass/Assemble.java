b47a5687315d4f40975c50c73f0c64efbb993188
Park-JunHyung <schwazodiac@gmail.com>
2018-12-20 16:55:33 +0900

    Add Structures of Assemble class

--- src/myclass/Assemble.java
+++ src/myclass/Assemble.java
@@ -1,4 +1,41 @@
 package myclass;
 
 public class Assemble {
+    int LABELSIZE = 10;
+    private int instrCnt;
+    private char lineBuffer[];
+    private int bufIndex;
+    Label labelProcess;
+    private char label[];
+    public int startAddr;
+
+    public Assemble() {
+        instrCnt = 0;
+    }
+
+    private void getLabel() {
+        
+    }
+
+    private int getOpcode() {
+        return 0;
+    }
+
+    private int getOperand() {
+        return 0;
+    }
+
+    private void instrWrite() {
+
+    }
+
+    public void assemble() {
+
+    }
+
+    public int startAddr() {
+        return 0;
+    }
+
+
 }
