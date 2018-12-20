e750e5edabe6c3eb3edca92be1d123070a7a0508
Jeong Seok Hyeon <arcane1028@gmail.com>
2018-12-20 18:49:47 +0900

    Add Interpret in complete

--- src/myclass/Interpret.java
+++ src/myclass/Interpret.java
@@ -1,4 +1,44 @@
 package myclass;
 
 public class Interpret {
+    private UcodeStack stack;
+    private int arBase;
+    private long tcycle;
+    private long exeCount;
+
+    public Interpret() {
+        this.stack = new UcodeStack(UcodeStack.STACKSIZE);
+        this.arBase = 4;
+        this.tcycle = 0;
+        this.exeCount = 0;
+    }
+
+    public void execute(int startAddress) {
+
+
+    }
+
+    private void predefinedProcess(int processIndex) {
+
+    }
+
+    private int findAddress(int n) {
+        int temp;
+        if (Label.instructionBuffer[n].value1 == 0) {
+            System.out.println("findAddr() " + "Lexical level is zero ...");
+            System.exit(1);
+        } else if (Label.instructionBuffer[n].value2 < 0) {
+            System.out.println("findAddr() " + "Negative offset ...");
+            System.exit(1);
+        }
+
+
+        return -1;
+    }
+
+    private void statistic() {
+
+    }
+
+
 }
