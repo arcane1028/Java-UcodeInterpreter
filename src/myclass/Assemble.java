daaed3651d3dbbb63aa84af4ffd44a9fc96d7249
Park-JunHyung <schwazodiac@gmail.com>
2018-12-21 14:43:20 +0900

    Implementation of Assemble instrWrite()

--- src/myclass/Assemble.java
+++ src/myclass/Assemble.java
@@ -1,6 +1,13 @@
 package myclass;
 
 import myenum.Opcode;
+import myinterpreter.UcodeInterpreter;
+import struct.Instruction;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Formatter;
 
 import static myinterpreter.UcodeInterpreter.LABELSIZE;
 import static myinterpreter.UcodeInterpreter.opcodeName;
@@ -12,6 +19,10 @@ public class Assemble {
     Label labelProcess;
     private char label[];
     public int startAddr;
+
+    private BufferedReader inputFile = UcodeInterpreter.inputFile;
+    private BufferedWriter outputFile = UcodeInterpreter.outputFile;
+    private Formatter formatter;
 
     public Assemble() {
         instrCnt = 0;
@@ -36,11 +47,11 @@ public class Assemble {
             mnemonic[index++] = lineBuffer[bufIndex++];
         mnemonic[index] = '\0';
 
-        for (index = Opcode.notop.ordinal(); index< Opcode.none.ordinal(); index++)
+        for (index = Opcode.notop.ordinal(); index < Opcode.none.ordinal(); index++)
             if (String.valueOf(mnemonic).equals(opcodeName[index]))
                 break;
-        if (index== Opcode.none.ordinal())
-            System.err.println("error Illegal Opcode, "+mnemonic);
+        if (index == Opcode.none.ordinal())
+            System.err.println("error Illegal Opcode, " + mnemonic);
         return index;
     }
 
@@ -49,22 +60,54 @@ public class Assemble {
 
         while (Character.isSpaceChar(lineBuffer[bufIndex]))
             bufIndex++;
-        result=0;
-        while (Character.isDigit(lineBuffer[bufIndex])&&lineBuffer[bufIndex]!='\n')
-            result=10*result+(lineBuffer[bufIndex++]-'0');
+        result = 0;
+        while (Character.isDigit(lineBuffer[bufIndex]) && lineBuffer[bufIndex] != '\n')
+            result = 10 * result + (lineBuffer[bufIndex++] - '0');
         return result;
     }
 
     private void instrWrite() {
         int i, j;
         char ch;
-        // TODO dodo
-
+        formatter = new Formatter();
+        try {
+            outputFile.write("\\n line       object           ucode  source program\\n\\n");
+            for (i = 1; i <= instrCnt; i++) {
+                formatter.format("%-5s", i + "    (");
+                formatter.format("%-2d", UcodeInterpreter.instructionBuffer[i].opcode);
+                j = UcodeInterpreter.instructionBuffer[i].opcode;
+                if (j == Opcode.chkl.ordinal() || j == Opcode.chkh.ordinal() ||
+                        j == Opcode.ldc.ordinal() || j == Opcode.bgn.ordinal() ||
+                        j == Opcode.ujp.ordinal() || j == Opcode.call.ordinal() ||
+                        j == Opcode.fjp.ordinal() || j == Opcode.tjp.ordinal()) {
+                    formatter.format("%-5d     ", UcodeInterpreter.instructionBuffer[i].value1);
+                } else if (j == Opcode.lod.ordinal() || j == Opcode.str.ordinal() || j == Opcode.lda.ordinal() ||
+                        j == Opcode.sym.ordinal() || j == Opcode.proc.ordinal()) {
+                    formatter.format("%-5d", UcodeInterpreter.instructionBuffer[i].value1);
+                    formatter.format("%-5d", UcodeInterpreter.instructionBuffer[i].value2);
+                }else {
+                    formatter.format("          ");
+                }
+                formatter.format(")     ");
+                outputFile.write(formatter.toString());
+                ch= (char) inputFile.read();
+                while (ch !='\n'){
+                    outputFile.write(ch);
+                    ch=(char) inputFile.read();
+                }
+                outputFile.write("\n");
+            }
+            outputFile.write("\n\n\n   ****    Result    ****\n\n\n");
+            formatter.flush();
+            formatter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void assemble() {
-        boolean done=false;
-        boolean end=false;
+        boolean done = false;
+        boolean end = false;
         int n;
 
         System.out.println(" == Assembling ... ===");
